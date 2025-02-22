1e6867bd7e6dc34a1261471dc55a5566613cc70c
Lexert19 <dominikch19@gmail.com>
2025-02-22 21:16:18 +0100

    parser

--- src/main/resources/static/js/htmlParser.js
+++ src/main/resources/static/js/htmlParser.js
@@ -10,62 +10,68 @@ class HtmlParser{
 
 
     parse(textFragment){
+        if (this.isThinkingBlock) {
+            this.readThinkElement(textFragment);
+            return;
+        }
+        if (this.isCodeBlock) {
+            this.readCodeBlock(textFragment);
+            return;
+        }
 
-        console.log(textFragment);
-        this.currentLineBuffer += textFragment;
-        let lines = this.currentLineBuffer.split('\n');
-        this.currentLineBuffer = lines.pop(); 
+        if (textFragment.startsWith("```")) {
+            const codeBlock = { type: 'code', language: false, text: '' };
+            if( !textFragment.includes("\n")){
+                codeBlock.language = true;
+            }
+            this.elements.push(codeBlock);
+            this.isCodeBlock = true;
+            return;
+        }
 
+        if (textFragment.startsWith("<think>")) {
+            this.currentThinkingBlock = { type: 'thinking', text: '' };
+            this.elements.push(this.currentThinkingBlock);
+            this.isThinkingBlock = true;
+            return;
+        }
         
 
-        for (const line of lines) {
+        // for (const line of lines) {
 
-            if (this.isCodeBlock) {
-                this.readCodeBlock(line);
-                continue; 
-            }
+          
 
-            if (this.isThinkingBlock) {
-                this.readThinkElement(line);
-                continue; 
-            }
+           
 
 
-            if (line.startsWith("```")) {
-                const language = line.substring(3).trim();
-                this.isCodeBlock = true;
-                this.currentCodeBlock.language = language;
-                continue;
-            }
+        //     if (line.startsWith("```")) {
+        //         const language = line.substring(3).trim();
+        //         this.isCodeBlock = true;
+        //         this.currentCodeBlock.language = language;
+        //         continue;
+        //     }
 
-            if (line.startsWith("<think>")) {
-                this.currentThinkingBlock = { type: 'thinking', text: '' };
-                this.elements.push(this.currentThinkingBlock);
-                this.isThinkingBlock = true;
-                continue;
-            }
+           
 
 
-            if (line.startsWith("###")) {
-                this.elements.push({ type: 'header', text: line.substring(3).trim() });
-            } else if (/^\d+\.\s\*\*(.+?)\*\*\:\s(.*)$/.test(line)) {
-                this.readOlElement(line);
-            } else if (line.startsWith("-")) {
-                this.elements.push({ type: 'ulElement', text: line.substring(1).trim() });
-            }
-            else if (line.trim() !== "") { 
-                this.elements.push({ type: 'normalText', text: line });
-            }
-        }
+        //     if (line.startsWith("###")) {
+        //         this.elements.push({ type: 'header', text: line.substring(3).trim() });
+        //     } else if (/^\d+\.\s\*\*(.+?)\*\*\:\s(.*)$/.test(line)) {
+        //         this.readOlElement(line);
+        //     } else if (line.startsWith("-")) {
+        //         this.elements.push({ type: 'ulElement', text: line.substring(1).trim() });
+        //     }
+        //     else if (line.trim() !== "") { 
+        //         this.elements.push({ type: 'normalText', text: line });
+        //     }
+        // }
     }
 
-    readCodeBlock(line){
-        if (line.trim() === "```") {
+    readCodeBlock(textFragment){
+        if(textFragment.startsWith("````")){
             this.isCodeBlock = false;
-            this.elements.push(this.currentCodeBlock);
-            this.currentCodeBlock = { type: 'code', language: '', text: '' };
-        } else {
-            this.currentCodeBlock.text += line + '\n';
+        }else{
+            this.elements[this.elements.length-1].text += textFragment;
         }
     }
 
@@ -73,20 +79,20 @@ class HtmlParser{
         if (line.trim() === "</think>") {
             this.isThinkingBlock = false;
         } else {
-            this.currentThinkingBlock.text += line + '\n';
+            this.elements[this.elements.length-1].text += textFragment;
         }
     }
 
-    readOlElement(line){
-        const match = line.match(/^(\d+)\.\s\*\*(.+?)\*\*\:\s(.*)$/);
-        if (match) {
-            const header = match[2].trim();
-            const text = match[3].trim();
-            this.elements.push({ type: 'olElement', header: header, text: text });
-        } else {
-            this.elements.push({ type: 'normalText', text: line });
-        }
-    }
+    // readOlElement(line){
+    //     const match = line.match(/^(\d+)\.\s\*\*(.+?)\*\*\:\s(.*)$/);
+    //     if (match) {
+    //         const header = match[2].trim();
+    //         const text = match[3].trim();
+    //         this.elements.push({ type: 'olElement', header: header, text: text });
+    //     } else {
+    //         this.elements.push({ type: 'normalText', text: line });
+    //     }
+    // }
 
     toHTML(){
         let html = "";
@@ -113,7 +119,7 @@ class HtmlParser{
                     break;
             }
         }
-        //console.log(html);
+        console.log(html);
         return html;
     }
 
