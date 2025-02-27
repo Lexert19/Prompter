c7e43c548244bc4a6adecef1af15e40610c0305d
Lexert19 <dominikch19@gmail.com>
2025-02-27 13:18:08 +0100

    zmiany interface

--- src/main/java/com/example/promptengineering/config/OAuth2Config.java
+++ src/main/java/com/example/promptengineering/config/OAuth2Config.java
@@ -3,6 +3,7 @@ package com.example.promptengineering.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
