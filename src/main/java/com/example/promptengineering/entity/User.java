0e0a1c339c0ba589fe0f0eb376ea71935dbde387
Lexert19 <dominikch19@gmail.com>
2025-03-05 21:07:43 +0100

    zmiany

--- src/main/java/com/example/promptengineering/entity/User.java
+++ src/main/java/com/example/promptengineering/entity/User.java
@@ -1,29 +1,20 @@
 package com.example.promptengineering.entity;
 
-import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.PostLoad;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 
 @Entity
 @Table(name = "users")
@@ -35,29 +26,8 @@ public class User implements OAuth2User {
     private String password;
 
     @Column(name = "keys", columnDefinition = "jsonb")
-    private String keys;
+    private HashMap<String, String> keys = new HashMap<>();
 
-    @Transient
-    private HashMap<String, String> keysMap = new HashMap<>();
-
-    @PrePersist
-    @PreUpdate
-    public void beforeSave() throws JsonProcessingException {
-        if (keysMap != null) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            this.keys = objectMapper.writeValueAsString(keysMap);
-        }
-    }
-
-    @PostLoad
-    public void afterLoad() throws IOException {
-        if (keys != null && !keys.isEmpty()) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            this.keysMap = objectMapper.readValue(keys,
-                    new TypeReference<HashMap<String, String>>() {
-                    });
-        }
-    }
 
     public User(String email, String password) {
         this.email = email;
@@ -101,12 +71,18 @@ public class User implements OAuth2User {
         this.password = password;
     }
 
-    public HashMap<String, String> getKeysMap() {
-        return keysMap;
+    public Long getId() {
+        return id;
     }
 
-    public void setKeysMap(HashMap<String, String> keys) {
-        this.keysMap = keys;
+    public HashMap<String, String> getKeys() {
+        return keys;
     }
+
+    public void setKeys(HashMap<String, String> keys) {
+        this.keys = keys;
+    }
+
+
 
 }
