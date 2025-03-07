46369db9aca0d0c88fb0c1079e938c5b52ff8c15
Lexert19 <dominikch19@gmail.com>
2025-03-07 21:19:46 +0100

    zmiany

--- src/main/java/com/example/promptengineering/entity/FileElement.java
+++ src/main/java/com/example/promptengineering/entity/FileElement.java
@@ -2,9 +2,9 @@ package com.example.promptengineering.entity;
 
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import jakarta.persistence.Id;
 
 @Document(collection = "fileElements") 
 public class FileElement {
