c01b02ed80d8f978546f84b00d2e6c8d32e0b8b0
Lexert19 <dominikch19@gmail.com>
2025-03-07 09:20:06 +0100

    zmiany

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -57,8 +57,8 @@ class Settings{
                 url: "https://api.deepinfra.com/v1/openai/chat/completions"
             }, 
             {
-                name: "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
-                text: "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
+                name: "Qwen/QwQ-32B",
+                text: "Qwen/QwQ-32B",
                 provider: "DEEPINFRA",
                 type: "text",
                 url: "https://api.deepinfra.com/v1/openai/chat/completions"
