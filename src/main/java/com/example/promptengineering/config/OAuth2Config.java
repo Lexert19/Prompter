ab3bf76d3c40f8f37859936e05c9e70992e3fe6a
Lexert19 <dominikch19@gmail.com>
2025-04-07 19:47:17 +0200

    config fix

--- src/main/java/com/example/promptengineering/config/OAuth2Config.java
+++ src/main/java/com/example/promptengineering/config/OAuth2Config.java
@@ -3,9 +3,6 @@ package com.example.promptengineering.config;
 import com.example.promptengineering.component.CustomAuthenticationEntryPoint;
 import com.example.promptengineering.component.CustomAuthenticationManager;
 import com.example.promptengineering.service.CustomOAuth2UserService;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -14,18 +11,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.web.*;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
@@ -47,9 +38,11 @@ public class OAuth2Config implements WebMvcConfigurer   {
         http.oauth2Login(oauth2 -> oauth2
                 .loginPage("/auth/login")
                 .userInfoEndpoint(userInfo -> userInfo
+                        .oidcUserService(oidcUserService())
                         .userService(customOAuth2UserService))
                 .successHandler((request, response, authentication) -> {
-                    response.sendRedirect("/");
+                    String serverName = request.getServerName();
+                    response.sendRedirect("http://" + serverName + ":8080/");
                 }));
 
         http.authenticationManager(authenticationManager);
@@ -62,15 +55,12 @@ public class OAuth2Config implements WebMvcConfigurer   {
                 .authenticationEntryPoint(authenticationEntryPoint())
         );
         http.logout(customizer -> customizer.logoutUrl("/auth/logout"));
-        //http.portMapper(portMapper -> portMapper.portMapper(portMapper()));
         return http.build();
     }
 
     @Bean
-    public PortMapper portMapper() {
-        PortMapperImpl portMapper = new PortMapperImpl();
-        portMapper.setPortMappings(Collections.singletonMap("8443", "8080"));
-        return portMapper;
+    public OidcUserService oidcUserService() {
+        return new OidcUserService();
     }
 
 
