d796a2e755b612711497f34f505fd301d8c93a6d
Lexert19 <dominikch19@gmail.com>
2026-03-03 18:27:52 +0100

    change role fix

--- src/main/java/com/example/promptengineering/controller/AdminController.java
+++ src/main/java/com/example/promptengineering/controller/AdminController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,7 +35,9 @@ public class AdminController {
         Optional<User> optionalUser = userRepository.findById(id);
         if (optionalUser.isPresent()) {
             User user = optionalUser.get();
-            user.setRoles(List.of(role));
+            List<AppRole> roles = new ArrayList<>();
+            roles.add(role);
+            user.setRoles(roles);
             userRepository.save(user);
         }
         return "redirect:/admin/users";
